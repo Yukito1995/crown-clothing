ff4c0267918fba3801b65aceb89122e50943bbc0
unknown <yukitofrank@gmail.com>
2021-09-04 10:54:31 +0200

    Creamos el proyecto de react y agregamos los ficheros del hompage

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import HomPage from './hompage.component';
+
+function App() {
+  return (
+    <div className="App">
+        <HomPage />
+    </div>
+  );
+}
+export default App;
---
+++ src/hompage.component.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const HomPage = () => (
+    <div className ='hompage'>
+     <div className ='directry-manu'>
+            <div className ='menu-item'>
+                <div className ='content'>
+                    <h1 className ='title'>HATS</h1>
+                    <samp className ='subtitle'>SHOPS NOW</samp>
+                </div>
+                <div className ='menu-item'>
+                <div className ='content'>
+                    <h1 className ='title'>JAKETS</h1>
+                    <samp className ='subtitle'>SHOPS NOW</samp>
+                </div>
+            </div> <div className ='menu-item'>
+                <div className ='content'>
+                    <h1 className ='title'>SNAKERS</h1>
+                    <samp className ='subtitle'>SHOPS NOW</samp>
+                </div>
+            </div> <div className ='menu-item'>
+                <div className ='content'>
+                    <h1 className ='title'>WOMANS</h1>
+                    <samp className ='subtitle'>SHOPS NOW</samp>
+                </div>
+            </div> <div className ='menu-item'>
+                <div className ='content'>
+                    <h1 className ='title'>MENS</h1>
+                    <samp className ='subtitle'>SHOPS NOW</samp>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+
+);
+export default HomPage;
